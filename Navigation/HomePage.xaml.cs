4ae74f9e1cd013a9e30c9a3e3cb5f760c803c369
Swetha <swetha.sf3977@gmail.com>
2024-10-09 13:08:06 +0530

    Updated Sample

---
+++ Navigation/HomePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Navigation;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Navigation/ProfilePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Navigation;
+
+public partial class ProfilePage : ContentPage
+{
+	public ProfilePage()
+	{
+		InitializeComponent();
+	}
+}
